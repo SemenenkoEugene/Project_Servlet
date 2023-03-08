2fb6d6945f5922150e4524464ad8c7d0841213f0
SemenenkoEugene <gks-08@mail.ru>
2023-03-08 17:20:58 +0300

    commit

---
+++ src/main/java/servlet/FlightServlet.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import service.FlightService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/flights")
+public class FlightServlet extends HttpServlet {
+
+    private final FlightService flightService = FlightService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        try (PrintWriter printWriter = resp.getWriter()) {
+            printWriter.write("<h1>Список перелетов:</h1>");
+            printWriter.write("<ul>");
+            flightService.findAll().forEach(flightDto -> {
+                printWriter.write("""
+                        <li>
+                            <a href="/tickets?flightId=%d">%s</a>
+                        </li>
+                        """.formatted(flightDto.getId(),flightDto.getDescription()));
+            });
+            printWriter.write("</ul>");
+        }
+    }
+}
